4ae2ae5b7b73ebd250b6efc8b39c5e44ff02a94d
Christie Lynam <christielynam@gmail.com>
2018-10-24 16:23:37 -0600

    Add loading gif

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import FilmScroll from '../FilmScroll/FilmScroll';
 import './App.css';
 import { fetchFilm } from '../../utils/apiCalls';
 import { cleanFilm } from '../../utils/cleaner';
+import loading from '../../assets/loading.gif';
 
 class App extends Component {
   constructor() {
@@ -24,14 +25,17 @@ class App extends Component {
     this.setState({film})
   }
 
-  
   render() {
     const { film } = this.state
     return (
       <div className="App">
         <h1 className='app-title'>SwapiBox</h1>
         <Route path='/' component={Nav} />
-        <Route exact path='/' render={() => <FilmScroll film={film} />} />
+        <Route exact path='/' render={() => (
+          Object.keys(film).length ? 
+          <FilmScroll film={film} /> :
+          <img src={loading} alt='loading gif' />
+        )} />
       </div>
     );
   }
